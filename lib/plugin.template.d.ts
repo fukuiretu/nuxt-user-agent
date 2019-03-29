70fe5819a80f31ec80cc99a5c89b2dc951ee8bf7
seiya-mogami <seiya.mogami@dena.com>
2019-03-29 19:15:35 +0900

    fix typo

--- lib/plugin.template.d.ts
+++ lib/plugin.template.d.ts
@@ -16,7 +16,7 @@ export declare class UA {
     isFromIos(): boolean;
     isFromAndroid(): boolean;
     isFromAndroidTablet(): boolean;
-    ifFromAndroidOs(): boolean;
+    isFromAndroidOs(): boolean;
     isFromWindowsPhone(): boolean;
     isFromPc(): boolean;
     isFromSmartphone(): boolean;
